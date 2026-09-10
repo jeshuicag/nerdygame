bed03bad8c031f79a33328b728e2bcb8a346f50b
jeshuicag <157768019+jeshuicag@users.noreply.github.com>
2026-09-09 18:20:34 -0700

    Add files via upload
    page for main progression tracking and page for grocery shopping mechanic

---
+++ nerdygamemain.py
@@ -0,0 +1,10 @@
+
+## track where players are in each mechanic
+mech_levels = {}
+
+## initialize levels
+mechs["grocshop"] = 0
+mechs["snakehunt"] = 0
+mechs["cashier"] = 0
+mechs["party"] = 0
+mechs["pizza"] = 0
