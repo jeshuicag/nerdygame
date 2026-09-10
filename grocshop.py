cddf8844b93100ec22efbdc2bb3f8fad81dd9b77
jeshuicag <157768019+jeshuicag@users.noreply.github.com>
2026-09-09 18:40:43 -0700

    Update grocshop.py
    add list of possible items exactly as their image is named

--- grocshop.py
+++ grocshop.py
@@ -1,3 +1,4 @@
 import random
 
+possible_items = ["pineapple", "onion", "olive", "bellpepper", "mushroom", "butter", "cheese", "flour", "garlic", "herbs", "ketchup", "salt", "sugar", "yeast", "oil"]
 random.randint(1,20)
